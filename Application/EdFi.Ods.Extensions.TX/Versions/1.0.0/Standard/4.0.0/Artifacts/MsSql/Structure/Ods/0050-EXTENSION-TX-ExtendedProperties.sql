cb3bad34d9c296cda04f3157742b30d82fe3f83d
ea-mtenhoor <mtenhoor@edanalytics.org>
2024-07-09 11:09:05 -0500

    Copy to 1.0.0

--- Application/EdFi.Ods.Extensions.TX/Versions/1.0.0/Standard/4.0.0/Artifacts/MsSql/Structure/Ods/0050-EXTENSION-TX-ExtendedProperties.sql
+++ Application/EdFi.Ods.Extensions.TX/Versions/1.0.0/Standard/4.0.0/Artifacts/MsSql/Structure/Ods/0050-EXTENSION-TX-ExtendedProperties.sql
@@ -1,3 +1,8 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
 -- Extended Properties [tx].[AcceleratedInstructionSubjectDescriptor] --
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'E3084. AcceleratedInstructionSubject indicates the subject in which the student is eligible to receive accelerated instruction.', @level0type=N'SCHEMA', @level0name=N'tx', @level1type=N'TABLE', @level1name=N'AcceleratedInstructionSubjectDescriptor'
 GO
